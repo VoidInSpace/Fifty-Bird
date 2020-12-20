d4e9b5dbc68e9e41a0be2d7366a371a273a09533
VoidInSpace <amlagniton@iskolarngbayan.pup.edu.ph>
2020-12-20 13:07:53 +0800

    made the pipe intervals slightly random

--- states/PlayState.lua
+++ states/PlayState.lua
@@ -13,54 +13,47 @@ function PlayState:init()
     self.pipePairs = {}
     self.timer = 0
 
-  
     self.lastY = -PIPE_HEIGHT + math.random(80) + 20
+    self.pipeInterval = math.random(3) == 2 and 3 or 2
 end
 
 function PlayState:update(dt)
-   
     self.timer = self.timer + dt
-
- 
-    if self.timer > 2 then
-        
+    
+    if self.timer > self.pipeInterval then
+        self.pipeInterval =  math.random(3) == 2 and 3 or 2
         local y = math.max(-PIPE_HEIGHT + 10, 
             math.min(self.lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
         self.lastY = y
-
-   
-        table.insert(self.pipePairs, PipePair(y))
-
-       
         self.timer = 0
+
+        table.insert(self.pipePairs, PipePair(y))
     end
 
     for k, pair in pairs(self.pipePairs) do
-    
         pair:update(dt)
     end
-
 
     for k, pair in pairs(self.pipePairs) do
         if pair.remove then
             table.remove(self.pipePairs, k)
         end
     end
-
-  
+    
     self.bird:update(dt)
 
-    
     for k, pair in pairs(self.pipePairs) do
         for l, pipe in pairs(pair.pipes) do
             if self.bird:collides(pipe) then
-                gStateMachine:change('title')
+                sounds['explosion']:play()
+                sounds['hurt']:play()
             end
         end
     end
 
     if self.bird.y > VIRTUAL_HEIGHT - 15 then
-        gStateMachine:change('title')
+        sounds['explosion']:play()
+        sounds['hurt']:play()
     end
 end
 
@@ -70,4 +63,12 @@ function PlayState:render()
     end
 
     self.bird:render()
+end
+
+function PlayState:enter()
+    scrolling = true
+end
+
+function PlayState:exit()
+    scrolling = false
 end
